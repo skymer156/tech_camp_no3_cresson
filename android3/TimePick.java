10ad63e501a7f2b3154c19e090670f2cac07f4cb
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-05-09 11:48:12 +0900

    add timepicker

---
+++ android3/TimePick.java
@@ -0,0 +1,31 @@
+package com.example.bless_you;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.widget.TimePicker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class TimePick extends DialogFragment implements
+        TimePickerDialog.OnTimeSetListener{
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(),
+                (MainActivity)getActivity(), hour, minute, true);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
